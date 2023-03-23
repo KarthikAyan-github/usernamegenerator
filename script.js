c1bc397965130cd19f65e970266a6f1a84b2d855
KarthikAyan-github <128299638+KarthikAyan-github@users.noreply.github.com>
2023-03-23 11:02:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,73 +1,116 @@
-function generateUsernames() {
-  var name = document.getElementById("name").value;
-  var nickname = document.getElementById("nickname").value;
-  var birth_year = document.getElementById("birth_year").value;
-  var interests = document.getElementById("interests").value;
-  var hobbies = document.getElementById("hobbies").value;
 
-  var special_character = document.getElementById("special_character").value;
-  var emoji = document.getElementById("emoji").value;
-  var emojis = document.getElementById("emojis").value;
+
+function generateUsername(firstname, lastname, middlename, dob, emojis, emoji, nickname, hobbies, interests) {
+  let fields = [];
+  if (firstname) fields.push(firstname);
+  if (lastname) fields.push(lastname);
+  if (middlename) fields.push(middlename);
+  if (emojis) fields.push(emojis);
+  if (emoji) fields.push(emoji);
+  if (nickname) fields.push(nickname);
+  if (hobbies) fields.push(hobbies);
+  if (interests) fields.push(interests);
+
+  let selectedFields = [];
+  if (fields.length > 4) {
+    while (selectedFields.length < 4) {
+      let index = Math.floor(Math.random() * fields.length);
+      if (!selectedFields.includes(fields[index])) {
+        selectedFields.push(fields[index]);
+      }
+    }
+  } else {
+    selectedFields = fields;
+  }
+
+  let parts = selectedFields.concat(dob.split('-'));
+  for (let i = parts.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [parts[i], parts[j]] = [parts[j], parts[i]];
+  }
+  let username = parts.join('').toLowerCase();
+  if (Math.random() < 0.2) {
+    let specialChars = ['.', '_'];
+    let randomChar = specialChars[Math.floor(Math.random() * specialChars.length)];
+    let randomNumber = Math.floor(Math.random() * 100);
+    username = username + randomChar + randomNumber;
+  }
+  return username;
+}
+
+
+// Function to generate 20 random usernames based on the user inputs and display them on the html page
+function generateUsernames() {
+  let firstname = document.getElementById('firstname').value;
+  let lastname = document.getElementById('lastname').value;
+  let middlename = document.getElementById('middlename').value;
+  let dob = document.getElementById('dob').value;
+  let emoji = document.getElementById('emoji').value;
+  let emojis = document.getElementById('emojis').value;
+  let nickname = document.getElementById('nickname').value;
+  let hobbies = document.getElementById('hobbies').value;
+  let interests = document.getElementById('interests').value;
   
-  if (name === "" || nickname === "" || birth_year === "") {
+  if (firstname === "" || dob === "") {
     alert("Please fill out all mandatory fields!");
     return;
   }
-  var input_str = name + special_character + nickname + birth_year + interests + emoji + emojis + hobbies;
-  var variations = [];
-  variations.push(input_str.toLowerCase());
-  variations.push(input_str.toUpperCase());
-  variations.push(input_str.charAt(0).toUpperCase() + input_str.slice(1));
-  variations.push(input_str.split("").reverse().join(""));
-  variations.push(input_str.replace(/a/g, "@"));
-  variations.push(input_str.replace(/i/g, "!"));
-  variations.push(input_str.replace(/s/g, "$"));
-  variations.push(input_str.replace(/o/g, "0"));
-  variations.push(input_str.replace(/e/g, "€"));
-  variations.push(input_str.replace(/y/g, "¥"));
-  variations.push(input_str.replace(/c/g, "©"));
-  
-  
-  variations.push(input_str + emoji);
-  variations.push(input_str + emojis);
-  
-  var usernames = [];
-  var count = 0;
-  while (usernames.length < 20 && count < 100) {
-    var shuffled_variations = variations.slice();
-    shuffled_variations.sort(function() { return 0.5 - Math.random() });
-    var username = shuffled_variations.join("").slice(0, Math.floor(Math.random() * (input_str.length - 5 + 1)) + 5);
-    if (!usernames.includes(username)) {
-      usernames.push(username);
-    }
-    count++;
+
+  let usernames = [];
+  for (let i = 0; i < 20; i++) {
+    let username = generateUsername(firstname, lastname, middlename, dob, emojis, emoji, nickname, hobbies, interests);
+    usernames.push(username);
   }
-  var usernames_str = "";
-  if (usernames.length > 0) {
-    usernames_str += "<strong>Here are some unique and catchy username options:</strong><br>";
-    for (var i = 0; i < usernames.length; i++) {
-      var username_html = usernames[i] + ' <button onclick="copyUsername(this)">Copy</button><br>';
-      usernames_str += username_html;
-    }
-    usernames_str += '<br><button type="button" onclick="generateUsernames()">Generate More</button>';
-  } else {
-    usernames_str += "Sorry, we could not generate any unique and catchy usernames for you.";
+
+  let usernamesDiv = document.getElementById('usernames');
+  usernamesDiv.innerHTML = '<strong>Here are some unique and catchy username options:</strong>';
+  for (let i = 0; i < usernames.length; i++) {
+    let usernameDiv = document.createElement('div');
+    usernameDiv.innerHTML = usernames[i];
+    let copyButton = document.createElement('button');
+	copyButton.id = 'cbtnId';
+    copyButton.innerHTML = 'Copy';
+    copyButton.addEventListener('click', function() {
+      copyToClipboard(usernames[i]);
+    });
+    usernameDiv.appendChild(copyButton);
+    usernamesDiv.appendChild(usernameDiv);
   }
-  document.getElementById("usernames").innerHTML = usernames_str;
+
+  let generateMoreButton = document.createElement('button');
+  generateMoreButton.id = 'mbtnId';
+  generateMoreButton.classList.add('mBtn');
+  generateMoreButton.innerHTML = 'Generate More';
+  generateMoreButton.addEventListener('click', function() {
+    generateUsernames();
+  });
+  usernamesDiv.appendChild(generateMoreButton);
 }
 
-function copyUsername(button) {
-  var username = button.previousSibling.textContent.trim();
-  var tempInput = document.createElement('input');
-  tempInput.setAttribute('value', username);
-  document.body.appendChild(tempInput);
-  tempInput.select();
-  tempInput.setSelectionRange(0, 99999); /*For mobile devices*/
+
+// Function to copy the generated username to the clipboard
+function copyToClipboard(text) {
+  const el = document.createElement('textarea');
+  el.value = text;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  const selected =
+    document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
+  el.select();
   document.execCommand('copy');
-  document.body.removeChild(tempInput);
-  if (!document.execCommand('copy')) {
-    document.execCommand('cut');
+  document.body.removeChild(el);
+  if (selected) {
+    document.getSelection().removeAllRanges();
+    document.getSelection().addRange(selected);
   }
-  alert('Username copied to clipboard!');
+  alert(text +' is copied to clipboard..😉');
 }
 
+
+
+// Call the generateUsernames function when the "Generate" button is clicked
+document.getElementById("generateButton").addEventListener("click", function() {
+  generateUsernames();
+});
